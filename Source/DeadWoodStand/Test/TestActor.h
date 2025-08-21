2190a85bc0090b0addf79b93576334dade64dd31
jscally-leyo <jochenschalley@hotmail.com>
2025-08-21 16:20:08 +0200

    Project Files INIT

---
+++ Source/DeadWoodStand/Test/TestActor.h
@@ -0,0 +1,26 @@
+// © 2025 Leyodemus. Development Build. All rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TestActor.generated.h"
+
+UCLASS()
+class DEADWOODSTAND_API ATestActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATestActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
