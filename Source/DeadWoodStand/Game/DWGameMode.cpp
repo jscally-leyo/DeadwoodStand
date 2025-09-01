6c3ab9489d93e56b79790ba8ee4d51d72707c58b
jscally-leyo <jochenschalley@hotmail.com>
2025-09-01 18:49:17 +0200

    Added GameMode

---
+++ Source/DeadWoodStand/Game/DWGameMode.cpp
@@ -0,0 +1,3 @@
+// © 2025 Leyodemus. Development Build. All rights reserved
+
+#include "DWGameMode.h"
---
+++ Source/DeadWoodStand/Game/DWGameMode.h
@@ -0,0 +1,14 @@
+// © 2025 Leyodemus. Development Build. All rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework//GameModeBase.h"
+#include "DWGameMode.generated.h"
+
+UCLASS()
+class DEADWOODSTAND_API ADWGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+};
