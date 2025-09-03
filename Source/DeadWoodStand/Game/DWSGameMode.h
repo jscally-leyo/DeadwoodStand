244d9f4328b29469cb67f555e541b9fcc7ce0dd3
jscally-leyo <jochenschalley@hotmail.com>
2025-09-03 10:07:01 +0200

    Added new game mode via "add Unreal class" in Rider

---
+++ Source/DeadWoodStand/Game/DWSGameMode.h
@@ -0,0 +1,13 @@
+// © 2025 Leyodemus. Development Build. All rights reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DWSGameMode.generated.h"
+
+UCLASS()
+class DEADWOODSTAND_API ADWSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
